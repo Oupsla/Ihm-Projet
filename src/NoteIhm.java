bd586bc125c1e21fc4dac5eeea0bbdc22bfc661b
dhamann <hamann.denis@gmail.com>
2016-04-15 17:54:51 +0200

    NoteIHM reduction/augmentation des parties

--- src/NoteIhm.java
+++ src/NoteIhm.java
@@ -16,63 +16,174 @@ import com.sun.xml.internal.messaging.saaj.soap.JpegDataContentHandler;
  * 
  * Classe representant une note de cours
  * 
- * Doit apparaitre :
- *  - Plusieurs sections sur la note (titre, introduction, 1ere partie, ...)
- *  - Une fleche sur le cote droit pour acceder à la superNote
- *  - Un bouton minimiser sur chaque sections pour miniser la partie
- *  (celle-ci apparait donc sous une autre forme avec par exemple le debut de la section
- *  suivi de 3 petits points)
- *  - Un bouton permettant de partager une section
- *  - Un bouton permettant de partager toute sa note
+ * Doit apparaitre : - Plusieurs sections sur la note (titre, introduction, 1ere
+ * partie, ...) - Une fleche sur le cote droit pour acceder à la superNote - Un
+ * bouton minimiser sur chaque sections pour miniser la partie (celle-ci
+ * apparait donc sous une autre forme avec par exemple le debut de la section
+ * suivi de 3 petits points) - Un bouton permettant de partager une section - Un
+ * bouton permettant de partager toute sa note
  * 
  */
-public class NoteIhm extends JPanel{
+public class NoteIhm extends JPanel {
 
-	public NoteIhm(){
+	private static String titreChapite = "<h1>Chapître 1</h1>";
+	private static String titrePartie1 = "<h2>Partie I</h2>";
+	private static String textePartie1 = "<p>Saraceni tamen nec amici nobis umquam nec hostes optandi, ultro citroque discursantes quicquid inveniri poterat momento temporis parvi vastabant milvorum rapacium similes, qui si praedam dispexerint celsius, volatu rapiunt celeri, aut nisi impetraverint, non inmorantur.</p>";
+	private static String titrePartie2 = "<h2>Partie II</h2>";
+	private static String textePartie2 = "<p>Haec et huius modi quaedam innumerabilia ultrix facinorum impiorum bonorumque praemiatrix aliquotiens operatur Adrastia atque utinam semper quam vocabulo duplici etiam Nemesim appellamus: ius quoddam sublime numinis efficacis, humanarum mentium opinione lunari circulo superpositum, vel ut definiunt alii, substantialis tutela generali potentia partilibus praesidens fatis, quam theologi veteres fingentes Iustitiae filiam ex abdita quadam aeternitate tradunt omnia despectare terrena.</p>"
+			+ "							<p>Quibus ita sceleste patratis Paulus cruore perfusus reversusque ad principis castra multos coopertos paene catenis adduxit in squalorem deiectos atque maestitiam, quorum adventu intendebantur eculei uncosque parabat carnifex et tormenta. et ex is proscripti sunt plures actique in exilium alii, non nullos gladii consumpsere poenales. nec enim quisquam facile meminit sub Constantio, ubi susurro tenus haec movebantur, quemquam absolutum.</p>"
+			+ "							<p>Quo cognito Constantius ultra mortalem modum exarsit ac nequo casu idem Gallus de futuris incertus agitare quaedam conducentia saluti suae per itinera conaretur, remoti sunt omnes de industria milites agentes in civitatibus perviis.</p>";
+	private static String ligneHorizontale = "<hr>";
+
+	public NoteIhm() {
 		this.setLayout(new BorderLayout());
 		JEditorPane ep = new JEditorPane();
 		ep.setContentType("text/html");
-		ep.setText("<h1>\r\n\tQu\u2019apporte SwingStates ?\r\n</h1>\r\n\r\n<p>\r\n\tSwingStates permet de programmer l\u2019interaction en la d\u00E9crivant \u00E0\r\n\tl\u2019aide de machines \u00E0 \u00E9tats\r\n</p>\r\n<ul>\r\n\t<li>les machines sont sp\u00E9cifi\u00E9es en Java, au milieu du reste du code</li>\r\n\t<li>elles peuvent ensuite \u00EAtre attach\u00E9es \u00E0 un widget</li>\r\n</ul>\r\n<p>\r\nSwingStates fournit un canevas pour le dessin 2D\r\n</p>\r\n<ul>\r\n\t<li>les formes \u00E0 afficher sont ajout\u00E9es une \u00E0 une au canevas</li>\r\n\t<li>pour chacune d\u2019elle, on peut sp\u00E9cifier la g\u00E9om\u00E9trie, des attributs\r\ngraphiques, un parent et des tags</li>\r\n\t<li>SwingStates s\u2019occupe de l\u2019affichage et de la s\u00E9lection (picking), des\r\nanimations</li>\r\n</ul>\r\n<p>\r\n\tSwingStates int\u00E8gre aussi deux reconnaisseurs de gestes, mais c\u2019est\r\nune autre histoire\u2026\r\n</p>\r\n<p>\r\nSwingStates is a library that adds state machines to the Java Swing user interface toolkit. Unlike traditional approaches, which use callbacks or listeners to define interaction, state machines provide a powerful control structure and localize all of the interaction code in one place. SwingStates takes advantage of Java's inner classes, providing programmers with a natural syntax and making it easier to follow and debug the resulting code. SwingStates tightly integrates state machines, the Java language and the Swing toolkit. It reduces the potential for an explosion of states by allowing multiple state machines to work together. SwingStates can be used to add new interaction techniques to existing Swing widgets, to program new widgets by using the flexible and powerful SwingStates' Canvas and to control high-level dialogues.\r\n</p>\r\n");
+		ep.setText(titreChapite + titrePartie1 + textePartie1 + titrePartie2 + textePartie2);
 		this.add(ep, BorderLayout.CENTER);
-		
-		JButton expand1 = new JButton("-");
-		expand1.setMargin(new java.awt.Insets(0, 2, 0, 3));
-		expand1.setFocusable(false);
-		
-		expand1.addActionListener(new ActionListener() {
-			
+
+		JButton expandChap = new JButton("-");
+		expandChap.setMargin(new java.awt.Insets(0, 2, 0, 3));
+		expandChap.setFocusable(false);
+
+		JButton expandPart1 = new JButton("-");
+		expandPart1.setMargin(new java.awt.Insets(0, 2, 0, 3));
+		expandPart1.setFocusable(false);
+
+		JButton expandPart2 = new JButton("-");
+		expandPart2.setMargin(new java.awt.Insets(0, 2, 0, 3));
+		expandPart2.setFocusable(false);
+
+		JPanel p = new JPanel();
+		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+		p.add(Box.createRigidArea(new Dimension(0, 16)));
+		p.add(expandChap);
+		p.add(Box.createRigidArea(new Dimension(0, 25)));
+		p.add(expandPart1);
+		p.add(Box.createRigidArea(new Dimension(0, 86)));
+		p.add(expandPart2);
+		this.add(p, BorderLayout.WEST);
+
+		JButton superNote = new JButton(">");
+		superNote.setFocusable(false);
+		JPanel pDroit = new JPanel();
+		pDroit.setLayout(new BoxLayout(pDroit, BoxLayout.Y_AXIS));
+		pDroit.add(Box.createRigidArea(new Dimension(0, Main.hauteur / 2)));
+		pDroit.add(superNote);
+		this.add(pDroit, BorderLayout.EAST);
+
+		expandChap.addActionListener(new ActionListener() {
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				if (expand1.getText().equals("+")){
-					expand1.setText("-");
-					expand1.setMargin(new java.awt.Insets(0, 2, 0, 3));
-					ep.setText("<h1>\r\n\tQu\u2019apporte SwingStates ?\r\n</h1>\r\n\r\n<p>\r\n\tSwingStates permet de programmer l\u2019interaction en la d\u00E9crivant \u00E0\r\n\tl\u2019aide de machines \u00E0 \u00E9tats\r\n</p>\r\n<ul>\r\n\t<li>les machines sont sp\u00E9cifi\u00E9es en Java, au milieu du reste du code</li>\r\n\t<li>elles peuvent ensuite \u00EAtre attach\u00E9es \u00E0 un widget</li>\r\n</ul>\r\n<p>\r\nSwingStates fournit un canevas pour le dessin 2D\r\n</p>\r\n<ul>\r\n\t<li>les formes \u00E0 afficher sont ajout\u00E9es une \u00E0 une au canevas</li>\r\n\t<li>pour chacune d\u2019elle, on peut sp\u00E9cifier la g\u00E9om\u00E9trie, des attributs\r\ngraphiques, un parent et des tags</li>\r\n\t<li>SwingStates s\u2019occupe de l\u2019affichage et de la s\u00E9lection (picking), des\r\nanimations</li>\r\n</ul>\r\n<p>\r\n\tSwingStates int\u00E8gre aussi deux reconnaisseurs de gestes, mais c\u2019est\r\nune autre histoire\u2026\r\n</p>\r\n<p>\r\nSwingStates is a library that adds state machines to the Java Swing user interface toolkit. Unlike traditional approaches, which use callbacks or listeners to define interaction, state machines provide a powerful control structure and localize all of the interaction code in one place. SwingStates takes advantage of Java's inner classes, providing programmers with a natural syntax and making it easier to follow and debug the resulting code. SwingStates tightly integrates state machines, the Java language and the Swing toolkit. It reduces the potential for an explosion of states by allowing multiple state machines to work together. SwingStates can be used to add new interaction techniques to existing Swing widgets, to program new widgets by using the flexible and powerful SwingStates' Canvas and to control high-level dialogues.\r\n</p>\r\n");
+				if (expandChap.getText().equals("+")) {
+					expandChap.setText("-");
+					expandChap.setMargin(new java.awt.Insets(0, 2, 0, 3));
+					ep.setText(titreChapite + titrePartie1 + textePartie1 + titrePartie2 + textePartie2);
+					expandPart1.setVisible(true);
+					expandPart2.setVisible(true);
+					expandPart1.setText("-");
+					expandPart1.setMargin(new java.awt.Insets(0, 2, 0, 3));
+					expandPart2.setText("-");
+					expandPart2.setMargin(new java.awt.Insets(0, 2, 0, 3));
+				} else {
+					expandChap.setText("+");
+					expandChap.setMargin(new java.awt.Insets(0, 0, 0, 0));
+					ep.setText(titreChapite + ligneHorizontale);
+					expandPart1.setVisible(false);
+					expandPart2.setVisible(false);
+
 				}
-				else{
-					expand1.setText("+");
-					expand1.setMargin(new java.awt.Insets(0, 0, 0, 0));
-					ep.setText("<h1>\r\n\tQu\u2019apporte SwingStates ?\r\n</h1>\r\n\r\n<p>\r\n\tSwingStates permet de programmer l\u2019interaction en la d\u00E9crivant \u00E0\r\n\tl\u2019aide de machines \u00E0 \u00E9tats\r\n\t<hr>\r\n</p>\r\n<p>\r\nSwingStates fournit un canevas pour le dessin 2D\r\n</p>\r\n<ul>\r\n\t<li>les formes \u00E0 afficher sont ajout\u00E9es une \u00E0 une au canevas</li>\r\n\t<li>pour chacune d\u2019elle, on peut sp\u00E9cifier la g\u00E9om\u00E9trie, des attributs\r\ngraphiques, un parent et des tags</li>\r\n\t<li>SwingStates s\u2019occupe de l\u2019affichage et de la s\u00E9lection (picking), des\r\nanimations</li>\r\n</ul>\r\n<p>\r\n\tSwingStates int\u00E8gre aussi deux reconnaisseurs de gestes, mais c\u2019est\r\nune autre histoire\u2026\r\n</p>\r\n\r\n<p>\r\nSwingStates is a library that adds state machines to the Java Swing user interface toolkit. Unlike traditional approaches, which use callbacks or listeners to define interaction, state machines provide a powerful control structure and localize all of the interaction code in one place. SwingStates takes advantage of Java's inner classes, providing programmers with a natural syntax and making it easier to follow and debug the resulting code. SwingStates tightly integrates state machines, the Java language and the Swing toolkit. It reduces the potential for an explosion of states by allowing multiple state machines to work together. SwingStates can be used to add new interaction techniques to existing Swing widgets, to program new widgets by using the flexible and powerful SwingStates' Canvas and to control high-level dialogues.\r\n</p>");
+
+			}
+		});
+
+		expandPart2.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (expandPart2.getText().equals("+")) {
+					expandPart2.setText("-");
+					expandPart2.setMargin(new java.awt.Insets(0, 2, 0, 3));
+					String finale = titreChapite;
+					if (expandPart1.getText().equals("-")) {
+						finale += titrePartie1 + textePartie1 + titrePartie2 + textePartie2;
+					} else {
+						finale += titrePartie1 + ligneHorizontale + titrePartie2 + textePartie2;
+					}
+					ep.setText(finale);
+				} else {
+					expandPart2.setText("+");
+					expandPart2.setMargin(new java.awt.Insets(0, 0, 0, 0));
+					String finale = titreChapite;
+					if (expandPart1.getText().equals("-")) {
+						finale += titrePartie1 + textePartie1 + titrePartie2 + ligneHorizontale;
+					} else {
+						finale += titrePartie1 + ligneHorizontale + titrePartie2 + ligneHorizontale;
+					}
+					ep.setText(finale);
+
 				}
-				
+
 			}
 		});
 		
-		JButton expand2 = new JButton("-");
-		expand2.setMargin(new java.awt.Insets(0, 2, 0, 3));
-		expand2.setFocusable(false);
-		
-		JButton expand3 = new JButton("-");
-		expand3.setMargin(new java.awt.Insets(0, 2, 0, 3));
-		expand3.setFocusable(false);
-		
-		JPanel p = new JPanel();
-		p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
-		p.add(Box.createRigidArea(new Dimension(0,62)));
-		p.add(expand1);
-		p.add(Box.createRigidArea(new Dimension(0,65)));
-		p.add(expand2);
-		p.add(Box.createRigidArea(new Dimension(0,115)));
-		p.add(expand3);
-		this.add(p, BorderLayout.WEST);
+		expandPart1.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (expandPart1.getText().equals("+")) {
+					
+					JPanel p = new JPanel();
+					p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+					p.add(Box.createRigidArea(new Dimension(0, 16)));
+					p.add(expandChap);
+					p.add(Box.createRigidArea(new Dimension(0, 25)));
+					p.add(expandPart1);
+					p.add(Box.createRigidArea(new Dimension(0, 86)));
+					p.add(expandPart2);
+					NoteIhm.this.add(p, BorderLayout.WEST);
+					
+					expandPart1.setText("-");
+					expandPart1.setMargin(new java.awt.Insets(0, 2, 0, 3));
+					String finale = titreChapite;
+					if (expandPart2.getText().equals("-")){
+						finale += titrePartie1 + textePartie1 + titrePartie2 + textePartie2;
+					}
+					else {
+						finale += titrePartie1 + textePartie1 + titrePartie2 + ligneHorizontale;
+					}
+					ep.setText(finale);
+					
+				} else { //
+					
+					JPanel p = new JPanel();
+					p.setLayout(new BoxLayout(p, BoxLayout.Y_AXIS));
+					p.add(Box.createRigidArea(new Dimension(0, 16)));
+					p.add(expandChap);
+					p.add(Box.createRigidArea(new Dimension(0, 25)));
+					p.add(expandPart1);
+					p.add(Box.createRigidArea(new Dimension(0, 31)));
+					p.add(expandPart2);
+					NoteIhm.this.add(p, BorderLayout.WEST);
+					
+					
+					expandPart1.setText("+");
+					expandPart1.setMargin(new java.awt.Insets(0, 0, 0, 0));
+					String finale = titreChapite;
+					if (expandPart2.getText().equals("-")){
+						System.out.println("coucou");
+						finale += titrePartie1 + ligneHorizontale+ titrePartie2 + textePartie2;
+					}
+					else {
+						finale += titrePartie1 + ligneHorizontale + titrePartie2 + ligneHorizontale;
+					}
+					
+					ep.setText(finale);
+				}
+
+			}
+		});
+
 	}
 }
